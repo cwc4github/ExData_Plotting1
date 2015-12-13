bb3029c5a6c067df654742134c426a3b55cb7ba8
cwc4github <cwc4mail@gmail.com>
2015-12-13 06:40:01 -0800

    1st version

---
+++ plot3.R
@@ -0,0 +1,21 @@
+## Get all data
+fileName <- "household_power_consumption.txt"
+data_all <- read.table(fileName, header = TRUE, sep = ";", na.strings = "?", stringsAsFactors=FALSE)
+data_all$Date <- as.Date(data_all$Date, format="%d/%m/%Y")
+
+## Subset the date
+data <- subset(data_all, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_all)
+
+## Converting DateTime
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time))
+
+## Creating plot3.png
+png("plot3.png", width = 480, height = 480)
+plot(data$DateTime, data$Sub_metering_1, ylab = "Energy sub metering",
+     xlab = "", col = "black", type = "l")
+lines(data$DateTime, data$Sub_metering_2, col = "red")
+lines(data$DateTime, data$Sub_metering_3, col = "blue")
+legend("topright", lwd = 1, col = c("black", "red", "blue"),
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
